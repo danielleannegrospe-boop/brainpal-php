39c24a6c92875ce82a48ba240c7c2616742c4708
danielleannegrospe-boop <danielleannegrospe@gmail.com>
2026-09-17 18:34:04 +0800

    Add API root and health check endpoints

---
+++ health.php
@@ -0,0 +1,11 @@
+@'
+<?php
+
+http_response_code(200);
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode([
+    'status' => 'ok',
+    'message' => 'BrainPal API health check passed.'
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+'@ | Set-Content -Path .\health.php -Encoding UTF8
---
+++ index.php
@@ -0,0 +1,11 @@
+@'
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode([
+    'status' => 'ok',
+    'message' => 'BrainPal PHP API is online.',
+    'service' => 'brainpal-php'
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+'@ | Set-Content -Path .\index.php -Encoding UTF8
